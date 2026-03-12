e62df339fdc43a0fbbc8f92c1463f9fa5b794ba5
Saumya Shah <sm351623@dal.ca>
2026-03-12 00:54:50 -0300

    added latest job experience and overall bug fixes

--- src/profilePage/TopPicksRow.tsx
+++ src/profilePage/TopPicksRow.tsx
@@ -2,11 +2,9 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './TopPicksRow.css';
 import { 
-  FaPassport, 
   FaCode, 
   FaBriefcase, 
   FaCertificate, 
-  FaHandsHelping, 
   FaProjectDiagram, 
   FaEnvelope, 
   FaMusic, 
